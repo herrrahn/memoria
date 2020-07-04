32a0889a5f8961e5307ccbd972b80f74686a61b5
Rafael R Rahn <rafael.rahn@live.com>
2020-07-04 15:21:45 +0200

    Initial commit

---
+++ MemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  MemoryGame.swift
+//  memoria
+//
+//  Created by Rafael Rahn on 04.07.20.
+//
+
+import Foundation
+
+struct MemoryGame {
+    
+}
--- memoria/ContentView.swift
+++ memoria/ContentView.swift
@@ -2,14 +2,37 @@
 //  ContentView.swift
 //  memoria
 //
-//  Created by Elaine Klemz on 04.07.20.
+//  Created by Rafael Rahn on 04.07.20.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .foregroundColor(Color.green)
+            .padding()
+            .font(Font.largeTitle)
+
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("🐒")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
