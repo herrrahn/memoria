ba6366be662e95c43d37a9a7e20a5e68297b575d
Rafael R Rahn <rafael.rahn@live.com>
2020-07-04 13:24:49 +0200

    Initial Commit

---
+++ memoria/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  memoria
+//
+//  Created by Elaine Klemz on 04.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
